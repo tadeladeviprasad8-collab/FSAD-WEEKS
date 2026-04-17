28d6f4bece030d8067adc939d1f5394ef13fa117
tadeladeviprasad8-collab <tadeladeviprasad8@gmail.com>
2026-04-17 14:01:36 +0530

    Add files via upload

---
+++ Week9_ProductCrudJPA/src/main/java/com/example/demo/ProductCrudJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCrudJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCrudJpaApplication.class, args);
+	}
+
+}
